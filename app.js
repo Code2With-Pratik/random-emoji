ea779cc7e5a638e52b30eacccef0e9484682d1d5
Code2With-Pratik <work.pratik1204@gmail.com>
2024-06-13 08:42:05 +0530

    all files are uploaded as mu first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const emoji = document.querySelector("#emoji");
+
+const emojis = [
+    "😀",
+    "😁",
+    "😃",
+    "😎",
+    "🥰",
+    "😘",
+    "😍",
+    "😚",
+    "🤗",
+    "🫡",
+    "😫",
+    "🥱",
+    "😛",
+    "😝",
+    "😤",
+    "😰",
+    "🤫",
+    "🥸",
+    "🫣"
+];
+
+emoji.addEventListener('mouseover',() =>{
+    emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+});
